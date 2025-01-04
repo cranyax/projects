8e6f3ceaf0f3efa2e4b3de01c526f938db8185f4
Abhishek Gaware <www.abhin711@gmail.com>
2025-01-04 19:03:06 +0530

    initialized project with ts, express, jwt, and mongoose including types.

---
+++ secondBrain/src/index.ts
@@ -0,0 +1,35 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
+
+// const express = require("express");  // this require statement doesn't care about types
+
+const app = express();
+
+app.post("/api/v1/signup", (req, res) => {
+
+})
+
+app.post("/api/v1/signin", (req, res) => {
+    
+})
+
+app.post("/api/v1/content", (req, res) => {
+    
+})
+
+app.get("/api/v1/content", (req, res) => {
+    
+})
+
+app.delete("/api/v1/content", (req, res) => {
+    
+})
+
+app.post("/api/v1/brain/share", (req, res) => {
+    
+})
+
+app.get("/api/v1/brain/:shareLink", (req, res) => {
+    
+})
